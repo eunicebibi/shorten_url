a7e33791df7084a93190dcd37c628b2e86f15307
eunicebibi <agjay410@gmail.com>
2023-03-03 07:16:15 +0800

    feat: add handlebars and index

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
+const exphbs = require('express-handlebars');
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -21,9 +22,12 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 //首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 
