61010906ca71e42bbcc1d22d13ee94d2dae7f3fa
eunicebibi <agjay410@gmail.com>
2023-03-04 22:01:39 +0800

    feat:add post route and generate short url ,modify url.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
-const exphbs = require('express-handlebars');
-const Shorten_url = require('./models/shorten_url') 
+const exphbs = require('express-handlebars')
+const Url = require('./models/url') 
 
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
@@ -30,16 +30,81 @@ app.use(express.urlencoded({ extended: true }))
 
 //首頁路由
 app.get('/', (req, res) => {
-  Shorten_url.find() // 取出  model 裡的所有資料
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .then(shortenurls => res.render('index', { shortenurls })) // 將資料傳給 index 樣板
-    .catch(error => console.error(error)) // 錯誤處理
-})
+  res.render('index');
+});
 
-app.post('/', (req, res) => {
-  console.log('req.body', req.body)
-  res.render('index')
-})
+// 產生短網址亂碼的函數
+function generateShortUrl() {
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let shortUrl = '';
+
+// 產生 5 碼的短網址亂碼
+  for (let i = 0; i < 5; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    const randomChar = chars.charAt(randomIndex);
+    shortUrl += randomChar;
+  }
+
+  return shortUrl;
+}
+
+// 在 /shorten 路由中使用 generateShortUrl 函數來產生短網址亂碼
+app.post('/shorten', (req, res) => {
+  const originalUrl = req.body.url;
+  if (!originalUrl) {
+    res.status(400).send('Please enter a valid URL');
+    return;
+  }
+
+  let url;
+//讓同樣網站只會出現同樣短網址
+  Url.findOne({ originalUrl })
+    .then((foundUrl) => {
+      url = foundUrl;
+      if (url) {
+        res.render('shorten', { shortUrl: url.shortUrl });
+        return;
+      }
+
+      const _id = new mongoose.Types.ObjectId();
+      const shortUrl = generateShortUrl();
+
+      url = new Url({
+        _id,
+        shortUrl,
+        originalUrl,
+      });
+
+      return url.save();
+    })
+    .then(() => {
+      res.render('shorten', { shortUrl: url.shortUrl });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Server error');
+    });
+});
+
+// 按下縮短網址可傳送到原網址
+app.get('/:shortUrl', (req, res) => {
+  const shortUrl = req.params.shortUrl;
+
+  Url.findOne({ shortUrl })
+    .then((url) => {
+      if (!url) {
+        res.status(404).send('URL not found');
+        return;
+      }
+
+      res.redirect(url.originalUrl);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Server error');
+    });
+});
+
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
--- models/shorten_url.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const shortenUrlSchema = new Schema({
-  originalUrl: String,
-  shortUrl: String,
-})
-module.exports = mongoose.model('Shorten_url', shortenUrlSchema)
